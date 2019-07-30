9522e417c55fce0fd2a8665c319c5de7275f9960
Gnarly Narwhal <gnarlynarwhal8@gmail.com>
2019-07-30 14:03:52 -0700

    Added documentation

--- src/com/codeland/mine/Board.java
+++ src/com/codeland/mine/Board.java
@@ -28,11 +28,11 @@ package com.codeland.mine;
 
 public class Board {
 
-	private interface Action {
+	public interface Action {
 		void execute(int x, int y, Element[][] board);
 	}
 
-	private class Element {
+	public class Element {
 		public int state;
 		public int mines;
 
@@ -57,25 +57,24 @@ public class Board {
 	}
 
 	public static final int
-		STATE_UNPRESSED  = 0x00,
-		STATE_FLAGGED    = 0x01,
-		STATE_DEPRESSED  = 0x02,
-		STATE_PRESSED    = 0x03,
-		STATE_DEAD       = 0x04;
+		STATE_UNPRESSED  = 0x00, // Mine count hidden
+		STATE_FLAGGED    = 0x01, // Mine count hidden, but flagged as a mine
+		STATE_DEPRESSED  = 0x02, // Tile appears pressed, but mine count is hidden. Occurs while mouse is held down
+		STATE_PRESSED    = 0x03, // Mine count revealed
+		STATE_DEAD       = 0x04; // If a mine is pressed that tile is set as the dead tile
 
-	private static final int
-		MINE = 0x09;
+	private static final int MINE = 0x09; // The ID of a mine
 
 	private static final boolean
-		DEPRESS_SELF      = false,
-		DEPRESS_NEIGHBORS = true;
+		DEPRESS_SELF      = false, // If the tile itself was depressed. E.g. a tile that was unpressed was sent the depress trigger
+		DEPRESS_NEIGHBORS = true;  // If the tile's neighbors are being depressed. E.g. a tile that is pressed was sent the depress trigger
 
-	private boolean depressType;
-	private int pressX;
-	private int pressY;
-	private int flagged;
+	private boolean depressType; // The type of depression that occured { DEPRESS_SELF | DEPRESS_NEIGHBORS }
+	private int pressX;          // The x index of the depressed tile
+	private int pressY;          // The y index of the depressed tile
+	private int flagged;         // If type is DEPRESS_NEIGHBORS, the number of flags adjacent to the depressed tile
 
-	private Element[][] board;
+	private Element[][] board;   // The actual board state
 
 	public Board() {
 		pressX = -1;
@@ -84,55 +83,91 @@ public class Board {
 		load();
 	}
 
+	/**
+	 * Gets the width of the board
+	 *
+	 * @return Returns the width of the board
+	 */
 	public int width() {
 		return board.length;
 	}
 
+	/**
+	 * Gets the height of the board
+	 *
+	 * @return Returns the height of the board
+	 */
 	public int height() {
 		return board[0].length;
 	}
 
-	public int getState(int x, int y) {
-		return board[x][y].state;
-	}
-
-	public int getMines(int x, int y) {
-		return board[x][y].mines;
-	}
-
+	/**
+	 * Presses a tile
+	 *
+	 * @param x - The x coordinate of the tile to press
+	 * @param y - The y coordinate of the tile to press
+	 */
 	private void press(int x, int y) {
+		// Set the state of the pressed tile to pressed
 		board[x][y].state = STATE_PRESSED;
+		// If the tile had no adjacent mines and wasn't
+		// a mine itself press all of it's neighbors as well
 		if (board[x][y].mines == 0)
 			neighbors(x, y, this::bucketClear);
 	}
 
+	/**
+	 * Toggles the flag state on a tile
+	 *
+	 * @param x - The x coordinate of the tile to flag
+	 * @param y - The y coordinate of the tile to flag
+	 */
 	public void flag(int x, int y) {
 		switch (board[x][y].state) {
+			// If the tile is unpressed, flag it
 			case STATE_UNPRESSED: board[x][y].state = STATE_FLAGGED;   break;
+			// If the tile is flagged, unflag it
 			case STATE_FLAGGED:   board[x][y].state = STATE_UNPRESSED; break;
+			// If the tile is neither flagged nor unpressed, do nothing
 		}
 	}
 
+	/**
+	 * Depresses the tile at position x, y
+	 * Clears any previously depressed tiles if they haven't been released already
+	 *
+	 * @param x - The x coordinate of the tile to depress
+	 * @param y - The y coordinate of the tile to depress
+	 */
 	public void depress(int x, int y) {
+		// If a tile is already depressed clear it
 		if (pressX != -1 || pressY != -1)
 			clearDepression();
+		// Set the currently depressed tile index to x, y
 		pressX = x;
 		pressY = y;
 		switch (board[x][y].state) {
+			// If the tile is unpressed, depress it and set the depress type to DEPRESS_SELF
 			case STATE_UNPRESSED:
 				board[x][y].state = STATE_DEPRESSED;
 				depressType = DEPRESS_SELF;
 				break;
+			// If the tile is pressed, depress it's neighbors and set the depress type to DEPRESS_NEIGHBORS
 			case STATE_PRESSED:
+				// Reset the number of adjacent flags
 				flagged = 0;
+				// For each neighbor...
 				neighbors(x, y, (nx, ny, board) -> {
 					switch (board[nx][ny].state) {
+						// If the neighbor is unpressed, depress it
 						case STATE_UNPRESSED: board[nx][ny].state = STATE_DEPRESSED; break;
+						// If the neighbor is flagged, increment the number of adjacent flags
 						case STATE_FLAGGED:   ++flagged;                             break;
 					}
 				});
 				depressType = DEPRESS_NEIGHBORS;
 				break;
+			// If the tile is neither pressed nor unpressed, reset the depressed tile index and ignore it
 			default:
 				pressX = -1;
 				pressY = -1;
@@ -140,58 +175,106 @@ public class Board {
 		}
 	}
 
+	/**
+	 * Releases the tile at position x, y if it's depressed
+	 * If it's not depressed clear the active depression and reset the depressed tile index
+	 *
+	 * If the depression type is DEPRESS_SELF, set it's state to pressed
+	 * If the depression type is DEPRESS_NEIGHBOR and
+	 *     the number of adjacent flags is >= to the number of mine neighbors, set all depressed neigbors to pressed
+	 *     the number of adjacent flags is <  to the number of mine neighbors, set all depressed neigbors to unpressed
+	 *
+	 * @param x - The x coordinate of the tile to release
+	 * @param y - The y coordinate of the tile to release
+	 */
 	public void release(int x, int y) {
+		// If the tile that was depressed and the tile being released are different, reset the depressed tile index and ignore it
 		if (pressX != x || pressY != y)
 			clearDepression();
 		else {
+			// If the depression type is DEPRESS_SELF, press it
 			if (depressType == DEPRESS_SELF)
 				press(x, y);
-			else if (flagged >= getMines(x, y)){ // DEPRESS_NEIGHBORS -> PRESSED
+			// If the depression type is DEPRESS_NEIGHBORS and the adjacent flag count is enough, then press all depressed neighbors
+			else if (flagged >= board[x][y].mines){
 				neighbors(x, y, (nx, ny, board) -> {
 					if (board[nx][ny].state == STATE_DEPRESSED)
 						press(nx, ny);
 				});
 			}
-			else // DEPRESS_NEIGHBORS -> UNPRESSED
+			// If the depression type is DEPRESS_NEIGHBORS and the adjacent flag count is not enough, then unpress all the depressed neighbors
+			else
 				clearDepression();
+			// Reset the depressed tile index
 			pressX = -1;
 			pressY = -1;
 		}
 	}
 
+	/**
+	 * Resets the currently depressed tile(s) to unpressed
+	 */
 	private void clearDepression() {
+		// If the depress type is DEPRESS_SELF, unpress it
 		if (depressType == DEPRESS_SELF)
 			board[pressX][pressY].state = STATE_UNPRESSED;
-		else { // DEPRESS_NEIGHBORS
+		// If the depress type is DEPRESS_NEIGHBORS, unpress it's depressed neighbors
+		else {
 			neighbors(pressX, pressY, (x, y, board) -> {
 				if (board[x][y].state == STATE_DEPRESSED)
 					board[x][y].state = STATE_UNPRESSED;
 			});
 		}
+		// Reset the depressed tile index
 		pressX = -1;
 		pressY = -1;
 	}
 
+	/**
+	 * Iterate over the entire board and perform the action and performs the action passed
+	 *
+	 * @param action - The action to perform on each tile
+	 */
 	public void iterate(Action action) {
 		for (int i = 0; i < width(); ++i)
 			for (int j = 0; j < height(); ++j)
 				action.execute(i, j, board);
 	}
 
+	/**
+	 * Iterates over each neighbor of a tile and performs the action passed
+	 *
+	 * @param x - The x coordinate of the tile whose neighbors to iterate over
+	 * @param y - The y coordinate of the tile whose neighbors to iterate over
+	 * @param action - The action to perform on each neighbor
+	 */
 	private void neighbors(int x, int y, Action action) {
+		// Starting -1, -1 (top left) relative to the given tile
+		// go through each neighbor until 1, 1 is reached
 		for (int k = -1; k < 2; ++k) {
 			for (int k2 = -1; k2 < 2; ++k2) {
+				// If this is not the tile itself
 				if (k != 0 || k2 != 0) {
+					// Calculate the neighbor's coordinate
 					int nx = x + k;
 					int ny = y + k2;
+					// If the tile is not outside the bounds of the board
 					if (nx > -1 && nx < width()
 					 && ny > -1 && ny < height())
+						// Perform the action
 						action.execute(nx, ny, board);
 				}
 			}
 		};
 	}
 
+	/**
+	 * Bucket fill through the board and reveal all contiguous empty tile
+	 *
+	 * @param x - The x coordinate of the empty tile
+	 * @param y - The y coordinate of the empty tile
+	 * @param board - The board to bucket through
+	 */
 	private void bucketClear(int x, int y, Element[][] board) {
 		if (board[x][y].state == STATE_UNPRESSED) {
 			press(x, y);
@@ -200,17 +283,29 @@ public class Board {
 		}
 	}
 
+	/**
+	 * Load the board
+	 */
 	public void load() {
+		// Get the mine positions
 		boolean[][] mines = loadMines();
+		// Create the board
 		board = new Element[mines.length][mines[0].length];
+		// Set the initial state of the tile and copy the mine positions in
 		iterate((i, j, board) -> {
 			board[i][j] = new Element(
+				// All tiles start unpressed
 				STATE_UNPRESSED,
+				// If there is a mine here set the tile to a mine
+				// otherwise set it to 0 neighbors (determine later)
 				mines[i][j] ? MINE : 0
 			);
 		});
+		// Determine the number of mine neighbors each tile has
 		iterate((i, j, board) -> {
+			// If the tile is a mine
 			if (board[i][j].mines == MINE) {
+				// Increment all it's non bomb neighbors by 1
 				neighbors(i, j, (x, y, leBoard) -> {
 					if (leBoard[x][y].mines != MINE)
 						++leBoard[x][y].mines;
@@ -219,6 +314,13 @@ public class Board {
 		});
 	}
 
+	/**
+	 * Load the mine positions
+	 * false - no mine
+	 * true  - mine
+	 *
+	 * @return Returns an array of booleans which represents the mine layout
+	 */
 	private static boolean[][] loadMines() {
 		boolean[][] ret = new boolean[16][16];
 		for (int i = 0; i < ret.length; ++i)
--- src/com/codeland/mine/Panel.java
+++ src/com/codeland/mine/Panel.java
@@ -52,13 +52,13 @@ public class Panel {
 
 	private Board board;
 
-	private float minDim;
+	private float tileDim;
 
 	private float pressX;
 	private float pressY;
 
-	private int leftOffset;
-	private int upOffset;
+	private float leftOffset;
+	private float upOffset;
 
 	public Panel(Window window, Camera camera) {
 		this.window = window;
@@ -69,78 +69,113 @@ public class Panel {
 		pressX = -1;
 		pressY = -1;
 
+		// If the textures are unloaded load all the textures
 		if (unpressed == null) {
-			minDim = Math.min(
+			tileDim = Math.min(
 				camera.getWidth()  / board.width(),
 				camera.getHeight() / board.height()
 			);
 
-			unpressed = new TexRect(camera, "res/img/tiles/unpressed.png", 0, 0, 0, minDim, minDim, 0, false);
-			flagged   = new TexRect(camera, "res/img/tiles/flagged.png",   0, 0, 0, minDim, minDim, 0, false);
-			depressed = new TexRect(camera, "res/img/tiles/depressed.png", 0, 0, 0, minDim, minDim, 0, false);
-			dead      = new TexRect(camera, "res/img/tiles/mine-dead.png",      0, 0, 0, minDim, minDim, 0, false);
+			unpressed = new TexRect(camera, "res/img/tiles/unpressed.png", 0, 0, 0, tileDim, tileDim, 0, false);
+			flagged   = new TexRect(camera, "res/img/tiles/flagged.png",   0, 0, 0, tileDim, tileDim, 0, false);
+			depressed = new TexRect(camera, "res/img/tiles/depressed.png", 0, 0, 0, tileDim, tileDim, 0, false);
+			dead      = new TexRect(camera, "res/img/tiles/mine-dead.png",      0, 0, 0, tileDim, tileDim, 0, false);
 
 			nums = new TexRect[10];
 			for (int i = 0; i < nums.length - 1; ++i)
-				nums[i] = new TexRect(camera, "res/img/tiles/num-" + i + ".png", 0, 0, 0, minDim, minDim, 0, false);
-			nums[9] = new TexRect(camera, "res/img/tiles/mine.png", 0, 0, 0, minDim, minDim, 0, false);
+				nums[i] = new TexRect(camera, "res/img/tiles/num-" + i + ".png", 0, 0, 0, tileDim, tileDim, 0, false);
+			nums[9] = new TexRect(camera, "res/img/tiles/mine.png", 0, 0, 0, tileDim, tileDim, 0, false);
 		}
 	}
 
 	public void update() {
+		// If the window was resized...
 		if (window.wasResized()) {
+			// Reset the camera dimensions to match the window dimensions
 			camera.setDims(window.getWidth(), window.getHeight());
 
-			minDim = Math.min(
+			// Get the largest tile dimension that will allow the entire board to fit within the window
+			tileDim = Math.min(
 				camera.getWidth()  / board.width(),
 				camera.getHeight() / board.height()
 			);
 
-			unpressed.setDims(minDim, minDim);
-			  flagged.setDims(minDim, minDim);
-			depressed.setDims(minDim, minDim);
+			// Reset all the tile dimensions
+			unpressed.setDims(tileDim, tileDim);
+			  flagged.setDims(tileDim, tileDim);
+			depressed.setDims(tileDim, tileDim);
 
 			for (int i = 0; i < nums.length; ++i)
-				nums[i].setDims(minDim, minDim);
+				nums[i].setDims(tileDim, tileDim);
 		}
 
-		leftOffset = (int) ((window.getWidth()  - (board.width()  * minDim)) / 2);
-		upOffset   = (int) ((window.getHeight() - (board.height() * minDim)) / 2);
+		// Calculate the render offsets from the top and bottom of the window
+		leftOffset = window.getWidth()  - (board.width()  * tileDim) / 2;
+		upOffset   = window.getHeight() - (board.height() * tileDim) / 2;
 
+		// If the left mouse button is pressed...
 		if (window.mousePressed(GLFW_MOUSE_BUTTON_1) >= Window.BUTTON_PRESSED)
+			// Trigger a depress action on the square that was clicked
 			checkBoardPress(board::depress);
+		// If the left mouse button was released this frame...
 		else if (window.mousePressed(GLFW_MOUSE_BUTTON_1) == Window.BUTTON_RELEASED)
+			// Trigger a release action on the square that was clicked
 			checkBoardPress(board::release);
+		// If the right mouse button was pressed this frame...
 		else if (window.mousePressed(GLFW_MOUSE_BUTTON_2) == Window.BUTTON_PRESSED)
+			// Trigger a flag action on the square that was clicked
 			checkBoardPress(board::flag);
 	}
 
+	/**
+	 * Renders the board centered in the window
+	 */
 	public void render() {
-		for (int i = 0; i < board.width(); ++i) {
-			for (int j = 0; j < board.height(); ++j) {
-				switch (board.getState(i, j)) {
-					case STATE_UNPRESSED: renderTile(leftOffset, upOffset, i, j, unpressed                 ); break;
-					case STATE_FLAGGED:   renderTile(leftOffset, upOffset, i, j, flagged                   ); break;
-					case STATE_DEPRESSED: renderTile(leftOffset, upOffset, i, j, depressed                 ); break;
-					case STATE_DEAD:      renderTile(leftOffset, upOffset, i, j, dead                      ); break;
-					case STATE_PRESSED:   renderTile(leftOffset, upOffset, i, j, nums[board.getMines(i, j)]); break;
-				}
+		// Render each tile in the board
+		board.iterate((x, y, board) -> {
+			// Calculate the position of the current tile
+			float xPos = leftOffset + x * tileDim;
+			float yPos = upOffset   + y * tileDim;
+			// Render the tile for that state at the position calculated
+			switch (board[x][y].state) {
+				case STATE_UNPRESSED: renderTile(xPos, yPos, unpressed              ); break;
+				case STATE_FLAGGED:   renderTile(xPos, yPos, flagged                ); break;
+				case STATE_DEPRESSED: renderTile(xPos, yPos, depressed              ); break;
+				case STATE_DEAD:      renderTile(xPos, yPos, dead                   ); break;
+				case STATE_PRESSED:   renderTile(xPos, yPos, nums[board[x][y].mines]); break;
 			}
-		}
+		});
 	}
 
+	/**
+	 * Checks whether the mouse lies within the bounds of the board.
+	 * If it does it executes <code>action</code> and passes it the x and y coordinates that the mouse lies on
+	 *
+	 * @param action - The action to exectue if the mouse lies within the board
+	 */
 	private void checkBoardPress(BoardPressAction action) {
+		// Get the mouse coordinates in camera space
 		Vector3f coords = window.getMouseCoords(camera);
-		int x = (int) ((coords.x - leftOffset) / minDim);
-		int y = (int) ((coords.y -   upOffset) / minDim);
+		// Get the position on the board that coordinate falls in
+		int x = (int) ((coords.x - leftOffset) / tileDim);
+		int y = (int) ((coords.y -   upOffset) / tileDim);
+		// If the position is within the bounds of the board execute the action
 		if (x > -1 && x < board.width()
 		 && y > -1 && y < board.height())
 			action.execute(x, y);
 	}
 
-	private static void renderTile(int left, int up, int x, int y, TexRect rect) {
-		float dims = rect.getWidth();
-		rect.setPosition(x * dims + left, y * dims + up);
+	/**
+	 * Renders a single tile of the board
+	 *
+	 * @param left - The left offset of the tile
+	 * @param up   - The right offset of the tile
+	 * @param rect - The tile to render
+	 */
+	private static void renderTile(float left, float up, TexRect rect) {
+		// Set the position of the tile
+		rect.setPosition(left, up);
+		// Render the tile
 		rect.render();
 	}
 }
